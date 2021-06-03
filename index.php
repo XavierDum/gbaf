9408e02b517470939bced756aa0c77773f2e4eb1
XavierDum <xavier.dumaine@orange.fr>
2021-06-03 14:50:27 +0200

    ajout index image et css

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/style.css" />
+		<title>Index</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+
+		<?php
+			include 'en_tete.php';
+		?>
+
+			<div id="presentation_gbaf">
+			<h1>Bienvenue sur GBAF</h1>
+
+			<p>L'Union européenne a adopté une série de directives en vue d’harmoniser les conditions d’exercice des activités bancaires dans les différents États membres. Parmi celles-ci, figure la directive MIF (directive relative aux marchés d’instruments financiers). Outre l’ouverture et la facilité d’accès aux marchés financiers, la directive MIF a pour objectif de renforcer les exigences réglementaires opposables aux prestataires d’investissement. Ceux-ci ont notamment l’obligation de classifier et d’informer leurs clients en fonction de leur nature et de leur connaissance des marchés financiers. Ils doivent ensuite s’assurer que les opérations qu’ils réalisent pour le compte de leur client correspondent bien à leur classification. Autant d’exigences qui nécessitent également de disposer d’une fonction de contrôle dédiée.</p>
+
+			<img src="images/illustration.jpg">
+			</div>
+
+			<section>
+				<h2>Présentation des acteurs</h2>
+				<p>L’exercice des activités bancaires, des services d’investissement et des services de paiement est réservé aux entreprises bénéficiant d’un agrément et soumis à une surveillance particulière par l’Autorité de Contrôle Prudentiel et de Résolution (ACPR). Globalement au 1er janvier 2012, on comptait 655 établissements de crédit agréés en France par l’Autorité de Contrôle Prudentiel et de Résolution.
+				</p>
+
+			</section>
+
+			<div id="partenaires">
+				liste intéractive des acteurs et partenaires
+			</div>
+
+			<?php
+			include 'pied_de_page.php';
+			?>
+
+		</div>
+	</body>
+
+</html>
