d4f85b42c8237c2065a886cfbd9f426ce339fa9e
XavierDum <xavier.dumaine@orange.fr>
2021-06-12 08:12:19 +0200

    page de confirmation de l'inscription

---
+++ confirmation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="" />
+		<title>confirmation</title>
+	</head>
+
+	<body>
+		<div id="bloc_page">
+			<section>
+				<h1>Utilisateur ajouté !</h1>
+				<a href="connexion.php"> se connecter </a>
+			</section>
+		</div>
+	</body>
+
+</html>
